83d5df754cb53747efbd0218ce35c4388cdbea2a
Aaron Bushnell <aaron.bushnell@gmail.com>
2020-06-09 09:46:41 -0400

    Align dashboard tables with CSS

--- src/assetbundles/isolate/dist/js/Isolate.js
+++ src/assetbundles/isolate/dist/js/Isolate.js
@@ -13,14 +13,9 @@
 class Isolate {
   constructor() {
     this.$mainNav = document.querySelector("#nav");
-    this.$isolateTables = document.querySelectorAll("[data-isolate-table]");
 
     this.moveIsolateNav();
     this.removeEntriesNav();
-
-    if (this.$isolateTables.length) {
-      this.getLargestCell();
-    }
   }
 
   removeEntriesNav() {
@@ -36,28 +31,6 @@ class Isolate {
 
     $isolateNav.parentNode.removeChild($isolateNav);
     $dashboardNav.insertAdjacentHTML("afterend", isolateNavHTML);
-  }
-
-  getLargestCell() {
-    let largestWidth = 0;
-
-    for (let i = 0; i < this.$isolateTables.length; i++) {
-      const $row = this.$isolateTables[i].querySelector("tbody tr td:first-of-type");
-      
-      if ($row.scrollWidth > largestWidth) {
-        largestWidth = $row.scrollWidth;
-      }
-    }
-
-    this.resizeRows(largestWidth);
-  }
-
-  resizeRows(cellWidth) {
-    const $rows = document.querySelectorAll("#content-container table tbody tr td:first-of-type");
-
-    for (let i = 0; i < $rows.length; i++) {
-      $rows[i].style.width = `${cellWidth}px`;
-    }
   }
 }
 
